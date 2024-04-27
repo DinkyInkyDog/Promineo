7f90fda275d97f70edb7be71fe303ac38d9f7479
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2024-04-27 18:35:30 -0400

    upload projects
    I finished my research project and figured I'd upload it to in order to keep track of my work.

---
+++ src/projects/Week04ResearchProjectCollectionsProblems.java
@@ -0,0 +1,28 @@
+package projects;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Week04ResearchProjectCollectionsProblems {
+
+	public static void main(String[] args) {
+		// 5. Write Java code that does the following:
+
+//(a) Write a line of code that shows how you would instantiate an ArrayList of String.
+		
+		List<String> stringArray = new ArrayList<String>();
+		//be sure to import the files that allow both the list and the array list to work.
+		
+//(b) Write a line of code that shows how you would instantiate a HashSet of StringBuilder.
+		Set<StringBuilder> stringBuildSet = new HashSet<StringBuilder>();
+		//again, import the files. And punctuation is important.
+		
+//(c) Write a line of code that shows how you would instantiate a HashMap of String, String.
+		Map<String, String> dictionary = new HashMap<String, String>();
+	}
+
+}
